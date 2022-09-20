5188e21c7fa2f90e51ad53421a7c7c17f3320297
Ashok <ashokmarmath8@gmail.com>
2022-09-20 18:39:02 +0530

    Update +page.ts

--- src/routes/tests/+page.ts
+++ src/routes/tests/+page.ts
@@ -2,15 +2,32 @@ import { getAPI } from '$lib/util/api'
 
 export async function load({ url, params, fetch, parent, setHeaders }) {
 	const { store } = await parent()
-	let faq
+	let faq, tcode
 	const res = await getAPI(`faqs?store=${store?.id}`)
+	const res1 = await getAPI(`short-code?store=${store?.id}`)
 	faq = res?.data[0]
+	tcode = res1?.data
 
-	const shortcode = await getShortCodeData('63218742139b1b312722cae8')
-	faq.answer = faq.answer.replace('[aaa]', shortcode)
+	var regex = /\[(.*?)\]/gm;
+
+	var regex1 = /[\[\]']+/gm;
+
+	const shorttag = JSON.stringify(faq.answer);
+	var shortco = shorttag.match(regex);
+	var shortcod = shortco.toString().replace(regex1, '');
+    console.log(shortcod);
+
+	const shortcode = await getShortCodeData('aaa')
+	faq.answer = faq.answer.replaceAll(shortco[1], shortcode)
+	const shortcode1 = await getShortCodeData('bbb')
+	faq.answer = faq.answer.replace(shortco[0], shortcode1)
+	
 
 	return { faq }
+	
+	
 }
+
 
 export const getShortCodeData = async (code) => {
 	const res = await getAPI(`short-code/${code}`)
